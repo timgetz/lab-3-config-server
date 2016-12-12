a5488b32decbf93d57b0bc0788b157d130283a8b
Tim Getz <timgetz@gmail.com>
2016-12-12 13:32:00 -0500

    Initial commit

---
+++ src/main/java/com/ms2/Lab3ConfigServerApplication.java
@@ -0,0 +1,14 @@
+package com.ms2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class Lab3ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab3ConfigServerApplication.class, args);
+	}
+}
